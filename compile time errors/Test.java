97d70db2da1130c52245d0907248e0383918fd26
famalone <finmalone@gmail.com>
2014-08-26 11:26:40 -0500

    Chapter 1 practice
    used the dialog boxes and URLs.

---
+++ compile time errors/Test.java
@@ -0,0 +1,14 @@
+import java.net.URL;
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+
+public class Test
+{
+    public static void main(String[] args) throws Exception
+    {
+        URL imageLocation = new URL(
+            "https://horstmann.com/java4everyone/duke.gif");
+        JOptionPane.showMessageDialog(null, "ALERT!!!", "ERROR",
+            JOptionPane.PLAIN_MESSAGE, new ImageIcon(imageLocation));
+        }
+    }
